85f77e7e5826e2221682131c16283e8d9e480133
Eliezer Marques Mafra <eliezermmafra@live.com>
2023-01-16 15:41:58 -0300

    feat: create Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { Header } from "./components/Header"
+import { Post } from "./Post"
+
+import './styles.css'
 
 function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+      <Post 
+        author='Eliezer'
+        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam consequuntur, sunt fuga doloribus dolor corrupti explicabo non magnam fugiat pariatur laudantium, eaque eos blanditiis voluptates tempora similique asperiores molestiae sapiente!'
+      />
+      <Post 
+        author='Paulo Muzi'
+        content='Bora BIIIIRRRRLLLL'
+      />
+    </div>
   )
 }
 
 export default App
+
