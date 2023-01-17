8559b42551fe29f9932d849fc32658356ddac4e3
Eliezer Marques Mafra <eliezermmafra@live.com>
2023-01-17 12:01:54 -0300

    feat: add image to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
+import igniteLogo from '../assets/ignite_logo.svg'
 
 export function Header(props) {
 	return (
 		<header className={styles.header}>
-			<strong>Ignite Feed</strong>
+			<img src={igniteLogo} alt="Logotipo do Ignite" />
 		</header>
 	)
 }
