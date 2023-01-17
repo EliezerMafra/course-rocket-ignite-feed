c943d276f56d5982092a4806b0fd521d4b5c6161
Eliezer Marques Mafra <eliezermmafra@live.com>
2023-01-17 13:31:15 -0300

    feat: add sidebar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
+import { Sidebar } from "./components/Sidebar"
 
 import './global.css'
+import styles from './App.module.css'
 
 function App() {
 
   return (
     <div>
       <Header />
-      <Post 
-        author='Eliezer'
-        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam consequuntur, sunt fuga doloribus dolor corrupti explicabo non magnam fugiat pariatur laudantium, eaque eos blanditiis voluptates tempora similique asperiores molestiae sapiente!'
-      />
-      <Post 
-        author='Paulo Muzi'
-        content='Bora BIIIIRRRRLLLL'
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Eliezer'
+            content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam consequuntur, sunt fuga doloribus dolor corrupti explicabo non magnam fugiat pariatur laudantium, eaque eos blanditiis voluptates tempora similique asperiores molestiae sapiente!'
+          />
+          <Post
+            author='Paulo Muzi'
+            content='Bora BIIIIRRRRLLLL'
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+	return (
+		<aside className={styles.sidebar}>
+			<img
+				className={styles.cover}
+				src="https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+			/>
+
+			<div className={styles.profile}>
+				<img
+					className={styles.avatar}
+					src="https://avatars.githubusercontent.com/u/32603174?v=4"
+				/>
+				<strong>Eliezer</strong>
+				<span>Web developer</span>
+			</div>
+
+			<footer>
+				<a href="#">
+					<PencilLine size={20}/>
+					Editar seu perfil
+				</a>
+			</footer>
+		</aside>
+	)
+}
